3900acbacf4fb6b7967147fac7960bc2543387d9
Jasmyn622 <shimzheng@student.unimelb.edu.au>
2022-12-02 19:12:27 +1100

    Create GameLevelNotFoundException.java

---
+++ Project2/GameLevelNotFoundException.java
@@ -0,0 +1,18 @@
+/**
+ * TODO: Write a comment describing your class here.
+ * @author TODO: Fill in your name, university email, and student number here.
+ * Name: Shiming ZHENG
+ * Email: shimzheng@student.unimelb.edu.au
+ * Student number: 1149897
+ */
+
+/**
+ * A defined class for GameLevelNotFoundException
+ */
+public class GameLevelNotFoundException extends Exception{
+    /**
+     * Capture the exception when the loaded file does not exist
+     */
+    public GameLevelNotFoundException(){}
+
+}
