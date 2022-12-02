bc0aa174b763f82a6c708a41cd0901bb34cf281e
Jasmyn622 <shimzheng@student.unimelb.edu.au>
2022-12-02 19:12:20 +1100

    Create Entity.java

---
+++ Project2/Entity.java
@@ -0,0 +1,29 @@
+/**
+ * TODO: Write a comment describing your class here.
+ * @author TODO: Fill in your name, university email, and student number here.
+ * Name: Shiming ZHENG
+ * Email: shimzheng@student.unimelb.edu.au
+ * Student number: 1149897
+ */
+
+/**
+ * A class for entities
+ */
+public class Entity {
+
+    /**
+     * the default constructor of the Entity class
+     */
+    public Entity() {
+    }
+
+    /**
+     * The function to print
+     *
+     * @param entity the type of the entity
+     */
+    void printEntity(String entity){
+        System.out.println("This is an entity.");
+    }
+
+}
